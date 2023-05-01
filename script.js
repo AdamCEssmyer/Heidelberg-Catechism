41cfcda6ed6c2b7073ca5f3ffa770c75e4b94bf0
Adam Essmyer <acemyer@gmail.com>
2023-05-01 12:46:49 -0500

    added 3rd catechism question

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ function unhide2() {
     hid[0].style.visibility = "visible";
   }
 }
+
+function unhide3() {
+  var hid = document.getElementsByClassName("exp3");
+  if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+    hid[0].style.visibility = "visible";
+  }
+}
