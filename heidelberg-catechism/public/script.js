b9774d1af404e500be7caafbe499ad995fe7cff6
Adam Essmyer <acemyer@gmail.com>
2023-05-05 11:22:13 -0500

    added react componenents

---
+++ heidelberg-catechism/public/script.js
@@ -0,0 +1,20 @@
+function unhide() {
+  var hid = document.getElementsByClassName("exp");
+  if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+    hid[0].style.visibility = "visible";
+  }
+}
+
+function unhide2() {
+  var hid = document.getElementsByClassName("exp2");
+  if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+    hid[0].style.visibility = "visible";
+  }
+}
+
+function unhide3() {
+  var hid = document.getElementsByClassName("exp3");
+  if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+    hid[0].style.visibility = "visible";
+  }
+}
