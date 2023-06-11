ae6fb98f41a9b060605bd15483fff65d8d5fd46e
Adam Essmyer <acemyer@gmail.com>
2023-06-11 15:46:09 -0500

    created text animation js file

---
+++ heidelberg-catechism/src/TextAnimation.js
@@ -0,0 +1,8 @@
+/*import React from "react"
+
+export default function TextAnimation(){
+   return <Wrapper>Heidelberg Catechism</Wrapper>
+}
+*/
+
+
--- heidelberg-catechism/src/index.js
+++ heidelberg-catechism/src/index.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import TextAnimation from './TextAnimation';
 import {createRoot} from 'react-dom/client';
 
 document.head.innerHTML = <div id="index"></div>
