f9dab46e52172db550696b297703dc533d78fbce
Adam Essmyer <acemyer@gmail.com>
2024-05-08 08:51:33 -0500

    updated react version and altered hide function

--- heidelberg-catechism/public/script.js
+++ heidelberg-catechism/public/script.js
@@ -3,8 +3,8 @@ function unhide() {
   var hid = document.getElementsByClassName("exp");
   if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
     hid[0].style.visibility = "visible";
- }else {
-  hid[0].style.display = "none";
+ }else{
+  hid.style.visibility = "none";
  }
 }
 
