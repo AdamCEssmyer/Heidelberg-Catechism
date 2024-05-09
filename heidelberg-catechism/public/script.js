d7db531ee9626ada490eeaa17bd2191c21d12693
Adam Essmyer <acemyer@gmail.com>
2024-05-09 09:37:05 -0500

    fixed unhide function for q5

--- heidelberg-catechism/public/script.js
+++ heidelberg-catechism/public/script.js
@@ -29,9 +29,11 @@ function unhide4() {
   }
 }
 
-function unhide5(){
+function unhide5() {
   const hid = document.getElementsByClassName("exp5");
-  if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0){
+  if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
     hid[0].style.visibility = "visible";
   }
 }
+
+
