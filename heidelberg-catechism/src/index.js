a4e45746504cebca4c76eb405a2614653c06adf1
Adam Essmyer <adamessmyer@Adams-MBP.lan1>
2024-05-26 21:50:16 -0500

    added style.css to src

--- heidelberg-catechism/src/index.js
+++ heidelberg-catechism/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+/*import React from 'react';
 import {createRoot} from 'react-dom/client';
 
 document.head.innerHTML = <div id="index"></div>
 
 const root = createRoot(document.getElementById('index'));
-root.render(<h1>"Heidelberg Catechism(1563)"</h1>);
+root.render(<h1>"Heidelberg Catechism(1563)"</h1>);*/
 
